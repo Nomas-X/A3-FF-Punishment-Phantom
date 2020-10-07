dee3625231666813dc2a7cab913911c6e1acc339
Caleb Serafin <calebserafin@outlook.com>
2020-10-07 22:08:52 +0200

    Added FF Code comparable to A3Antistasi,+Demo.

---
+++ EmptyMission/functions.hpp
@@ -0,0 +1,22 @@
+class FFPP
+{
+    class Punishment
+    {
+        class punishment_dataGet {};
+        class punishment_dataNamespace {};
+        class punishment_dataRem {};
+        class punishment_dataSet {};
+        class punishment_FF_addEH {};
+        class punishment_FF {};
+        class punishment {};
+    };
+    class UI
+    {
+        class customHint {};
+    };
+    class Utility
+    {
+        class getAdmin {};
+        class log {};
+    };
+};
