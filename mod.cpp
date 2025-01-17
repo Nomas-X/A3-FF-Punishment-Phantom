905108b20ab3e134bffc5b32324e8c6de39be6c0
Nomas <nomasx2000@gmail.com>
2025-01-17 15:43:56 +0300

    Create mod.cpp
    - Added the mod presentation file

---
+++ mod.cpp
@@ -0,0 +1,33 @@
+name = "Friendly Fire Phantom Protocol";
+
+author = "Antistasi Official Community";
+
+picture         = "img\FFPPLogo_2048x1024.paa";                                                                                               // Picture displayed from the expansions menu. Optimal size is 2048x1024
+hideName        = "false";                                                                                                                          // Hide the extension name
+hidePicture     = "false";                                                                                                                          // Hide the extension menu
+
+logoSmall       = "img\FFPPLogo_64x64.paa";                                                                                                    // Display next to the item added by the mod 64x64
+logo            = "img\FFPPLogo_128x128.paa";                                                                                                   // Logo displayed in the main menu 128x128
+logoOver        = "img\FFPPLogo_128x128.paa";                                                                                                     // When the mouse is over, in the main menu 128x128
+
+tooltip         = "Friendly Fire Phantom Protocol";
+tooltipOwned    = "Made by the Antistasi Community";                                                                                                          // Tool tip displayed when the mouse is left over, in the main menu
+overview        = "Friendly Fire Phantom Protocol - This mod is used to log and show friendly fire notifications.";                 // Overview text, displayed from the extension menu
+description     = "This mod is used to log and show friendly fire notifications.";                                                     // Probably in context with action
+action          = "http://discord.gg/antistasi/";                                                                                                // Website URL, that can accessed from the expansions menu 
+actionName      = "Discord";                                                                                                                        // label of button/tooltip in extension menu
+
+overviewPicture = "img\FFPPLogo_2048x1024.paa";
+overviewText = "Friendly Fire Phantom Protocol Mod";
+
+overviewFootnote = "Friendly Fire Phantom Protocol Mod";
+
+
+// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] =
+{
+    0.85,
+    0.85,
+    0.00,
+    1.00
+};
